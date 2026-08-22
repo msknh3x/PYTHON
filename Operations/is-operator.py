d07cf965d994ab19525d66139b2dd61afa2e8b80
Muskan <muskaan29899@gmail.com>
2026-08-22 20:23:52 +0500

    new files and folders added

--- Operations/is-operator.py
+++ Operations/is-operator.py
@@ -2,8 +2,4 @@
 # is operator
 x = None
 print(x is None)   # True
-git init
-git add .
-git commit -m "added all folders and files"
-git remote add origin https://github.com/YOUR-USERNAME/YOUR-REPO-NAME.git
-git push -u origin main
+
