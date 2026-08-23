bb4e6239f84c85eef86ab9f6a1962d5c6543ddb8
Muskan <muskaan29899@gmail.com>
2026-08-23 14:13:43 +0500

    added lists

---
+++ Lists/list_add.py
@@ -0,0 +1,7 @@
+fruits = ["apple", "banana"]
+
+fruits.append("cherry") # append adds item at the end
+print(fruits)
+
+fruits.insert(0, "orange") # insert add item at specific index
+print(fruits) 
---
+++ Lists/list_basic.py
@@ -0,0 +1,6 @@
+fruits = ["strawberry", "cherry", "mango"]
+
+print(fruits)
+print(fruits[0])
+print(fruits[1])
+print(fruits[2])
