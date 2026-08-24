8bbcdcefa58c90dd614be27389b4c7d2e5905866
Muskan <muskaan29899@gmail.com>
2026-08-24 14:35:43 +0500

    new files added

---
+++ sets/club_members.py
@@ -0,0 +1,6 @@
+coding_club = {"Soha", "Babar", "Sania"}
+sports_club = {"Maria", "Hafsa", "Umaima"}
+
+print("Union:", coding_club | sports_club)
+print("Intersection:", coding_club & sports_club)
+print("Difference:", coding_club - sports_club)
---
+++ sets/students_ids.py
@@ -0,0 +1,3 @@
+students_ids = {101, 282, 282, 129, 928}
+
+print(students_ids)
