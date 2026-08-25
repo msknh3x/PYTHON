7be351999a7fba05571bbbee89a6c68ff1cdac2a
Muskan <muskaan29899@gmail.com>
2026-08-25 18:56:37 +0500

    created atm_machine.py

---
+++ conditionals/atm_machine.py
@@ -0,0 +1,18 @@
+balance = 10000
+
+print("Welcome to ATM")
+choice = int(input("Enter 1 for Withdraw, 2 for Deposit: "))
+
+if choice == 1:
+    amount = int(input("Enter amount to withdraw: "))
+    if amount <= balance:
+        balance -= amount
+        print("Withdrawal successful! New Balance: ", balance)
+    else:
+        print("Insufficient funds!")
+elif choice == 2:
+    amount = int(input("Enter amoubt to deposit:"))
+    balance += amount
+    print("'Deposit successful! New balance:", balance)
+else:
+    print("Invalid choice")
