0c4ab0d1489487f84e48fd519a5bedb8487dc6f8
Muskan <muskaan29899@gmail.com>
2026-08-29 21:43:13 +0500

    updated login system

--- conditionals/LoginSystem.py
+++ conditionals/LoginSystem.py
@@ -1,5 +1,5 @@
-username = "Muskan"
-password = "1234"
+username = input("Enter username:")
+password = input("Enter password:")
 
 if username == "Muskan" and password == "1234":
     print("Login Successful")
