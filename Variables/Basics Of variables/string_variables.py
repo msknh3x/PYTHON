b5d42247de5c858c7d81e97a6f7dad09ec8d7c1a
mskn.h3x <muskaan29899@gmail.com>
2026-09-15 22:30:10 +0500

    created string_variable.py

---
+++ Variables/Basics Of variables/string_variables.py
@@ -0,0 +1,17 @@
+# storing text in string
+username = "cyber_ninja"
+
+# find the no of characters in the string
+username_length = len(username)
+
+# convert the string to uppercase
+uppercase_name = username.upper()
+
+# convert the string to lowercase
+lowercase_name = username.lower()
+
+# display the results
+print("username:", username)
+print("Length:", username_length)
+print("Uppercase:", uppercase_name)
+print("Lowercase:", lowercase_name)
